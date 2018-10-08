d6e6831ee52c7183224db357657f1f3038abb34a
Frank Fink <ffink@github.com>
2018-10-08 16:53:54 -0700

    Create trash-collector-pro.py

---
+++ trash-collector-pro.py
@@ -0,0 +1,20 @@
+import os
+import time
+import getpass
+import os.path
+
+
+def get_downloads_path():
+    user_id = getpass.getuser()
+    downloads_path = '/Users/' + user_id + '/Downloads'
+    return downloads_path
+
+
+def get_old_downloads():
+    all_downloads = os.listdir(get_downloads_path())
+    print('The following files were modified more than 30 days ago and will be removed:')
+    for file in all_downloads:
+        modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
+        days_old = (time.time() - modify_time) / 86400
+        if days_old > 30:
+            print(file)
