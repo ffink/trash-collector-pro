7d52c966ace9a83ddb1f8f05c57be23726c9407b
Frank Fink <ffink@github.com>
2018-10-12 14:15:38 -0700

    Deletes directories now, with addl user confirmation

--- clean_up.py
+++ clean_up.py
@@ -2,6 +2,7 @@ import os
 import time
 import getpass
 import os.path
+import shutil
 
 
 def get_downloads_path():
@@ -18,12 +19,11 @@ def get_downloads():
 def clean():
     print('The following downloads were last modified over 30 days ago and will be removed:')
     for file in get_downloads():
-        if os.path.isfile(os.path.join(get_downloads_path(),file)) == True:
-            modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
-            days_old = (time.time() - modify_time) / 86400
-            if days_old > 30:
-                print(file + ' | ' + str(round(days_old,1)) + ' days ago.')
-    response = input("Confirm delete? (y/n) ")
+        modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
+        days_old = (time.time() - modify_time) / 86400
+        if days_old > 30:
+            print(file + ' | ' + str(round(days_old,1)) + ' days ago.')
+    response = raw_input("Confirm delete? (y/n) ")
     if response == 'y' or response == 'yes':
         for file in get_downloads():
             if os.path.isfile(os.path.join(get_downloads_path(),file)) == True:
@@ -31,9 +31,21 @@ def clean():
                 days_old = (time.time() - modify_time) / 86400
                 if days_old > 30:
                     os.remove(os.path.join(get_downloads_path(),file))
+            elif os.path.isfile(os.path.join(get_downloads_path(),file)) == False:
+                modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
+                days_old = (time.time() - modify_time) / 86400
+                if days_old > 30:
+                    directory_response = raw_input('Warning: ' + file + 'is a directory. All contents will be deleted. Confirm? (y/n) ')
+                    if directory_response == 'y' or directory_response == 'yes':
+                        shutil.rmtree(os.path.join(get_downloads_path(),file))
+                    elif directory_response == 'n' or directory_response == 'no':
+                        print('Directory will not be removed.')
+                    else:
+                        print('Invalid entry. Directory will not be removed.')
     elif response == 'n' or response == 'no':
         print('Aborted.')
     else:
         print('Invalid response. Process aborted.')
+
 
 clean()
