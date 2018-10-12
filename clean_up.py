f062e96bd50b74a6e570c0b56ab41ec1aa0a5917
Frank Fink <ffink@github.com>
2018-10-12 11:28:08 -0700

    Actually deletes things now

--- clean_up.py
+++ clean_up.py
@@ -18,10 +18,22 @@ def get_downloads():
 def clean():
     print('The following downloads were last modified over 30 days ago and will be removed:')
     for file in get_downloads():
-        modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
-        days_old = (time.time() - modify_time) / 86400
-        if days_old > 30:
-            print(file + ' | ' + str(round(days_old,1)) + ' days ago.')
-
+        if os.path.isfile(os.path.join(get_downloads_path(),file)) == True:
+            modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
+            days_old = (time.time() - modify_time) / 86400
+            if days_old > 30:
+                print(file + ' | ' + str(round(days_old,1)) + ' days ago.')
+    response = input("Confirm delete? (y/n) ")
+    if response == 'y' or response == 'yes':
+        for file in get_downloads():
+            if os.path.isfile(os.path.join(get_downloads_path(),file)) == True:
+                modify_time = os.path.getmtime(os.path.join(get_downloads_path(),file))
+                days_old = (time.time() - modify_time) / 86400
+                if days_old > 30:
+                    os.remove(os.path.join(get_downloads_path(),file))
+    elif response == 'n' or response == 'no':
+        print('Aborted.')
+    else:
+        print('Invalid response. Process aborted.')
 
 clean()
